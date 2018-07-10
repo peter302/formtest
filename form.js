600a83884ccce3ea8dd54fd0dc90ca4fb5f74ad6
peter302 <petermbaik@gmail.com>
2018-07-10 11:12:20 +0300

    added a ccs file

---
+++ form.js
@@ -0,0 +1,12 @@
+$(document).ready(fuction(){
+  $(form).submmit(function(event){
+    var p=$(".come").val()
+    var j=$(".sawa").val()
+    var f=$(".pos").val()
+    $(".1").text("p")
+    $(".2").text("j")
+    $(".f").text("f")
+    $(".jk").show()
+    event.preventDefault
+  });
+});
